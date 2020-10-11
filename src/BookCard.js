f9baff505a1e3cec82b503623b77c0ee18556823
hanodims <hend--mohammed@outlook.com>
2020-10-11 15:20:41 +0300

    authors=>links=>detail page

--- src/BookCard.js
+++ src/BookCard.js
@@ -4,14 +4,28 @@ import { Link } from "react-router-dom";
 const BookCard = (props) => {
   const book = props.book;
   const bookTitle = `${book.title}`;
-  //  <Link to={`/authors/${author.id}`} className="card"> </Link>
+  const authors = book.authors.map((author) => (
+    <div key={author.id}>
+      {" "}
+      <Link to={`/authors/${author.id}`}>{author.name}</Link>
+    </div>
+  ));
+  //  <Link to={`/authors/${author.id}`}> </Link>
   return (
     <div className="col-lg-4 col-md-6 col-12">
-      <div className="card-body">
-        <h5 className="card-title">
-          <span>{bookTitle}</span>
-        </h5>
-        <button className="btn" style={{ backgroundColor: book.color }} />
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">
+            <span>{bookTitle}</span>
+          </h5>
+          <h5 className="card-title">
+            <span>Authors:</span>
+          </h5>{" "}
+          <h5 className="card-title">
+            <span>{authors}</span>
+          </h5>
+          <button className="btn" style={{ backgroundColor: book.color }} />
+        </div>
       </div>
     </div>
   );
